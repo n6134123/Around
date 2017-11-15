0a7086ce0b64b4b2cf605778fc92c3a60eab99dd
n6134123 <n6134123@gmail.com>
2017-11-15 12:36:50 -0800

    done the go tutorial

--- service/hwTest.go
+++ service/hwTest.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type element struct {
+	x int
+	y int
+}
+
+type Location struct {
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
+}
+
+type Post struct {
+	// `json:"user"` is for the json parsing of this User field. Otherwise, by default it's 'User'.
+	User     string `json:"user"`
+	Message  string  `json:"message"`
+	Location Location `json:"location"`
+}
+
+func ThisIsTest(arr []int) {
+	arr[0] = 1
+	arr[1] = 2
+
+}
+
 
 func main() {
-	fmt.Println("this is hw test");
+	//Homework 1, given a map from string to bool, how to iterate over it and print out all the key-values?
+	m := make(map[string]bool)
+	m["andy"] = true
+	m["monica"] = false
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
 }
